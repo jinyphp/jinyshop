5a4558ac6a820f2ca699f569cf114d22fc4cd284
ngj1129 <grw13141@naver.com>
2024-07-17 17:30:25 +0900

    메인페이지 컴포넌트 추가

--- resources/www/slot1/shop/home.blade.php
+++ resources/www/slot1/shop/home.blade.php
@@ -1,4 +1,14 @@
 <x-www-layout>
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <h1>카테고리</h1>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-6">
+            @livewire('ShopCategory')
+        </div>
+    </div>
     <div>@livewire('ShopBanner')</div>
     <div>
     </div>
@@ -10,26 +20,59 @@
 
     <div class="row justify-content-center">
         <div class="col-6">
-            <h1>할인 중 상품</h1>
-        </div>
-    </div>
-
-
-    <div class="row justify-content-center">
-        <div class="col-6">
-            @livewire('ShopEvent')
+            @livewire('LiveOnSale')
         </div>
     </div>
 
     <div class="row justify-content-center">
         <div class="col-6">
-            <h1>리뷰</h1>
+            <h1>매니저 정보</h1>
         </div>
     </div>
 
     <div class="row justify-content-center">
         <div class="col-6">
-            @livewire('ShopReview')
+            @livewire('ShopManager')
         </div>
     </div>
+
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <h1>판매자 정보</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-6">
+            @livewire('ShopSeller')
+        </div>
+    </div>
+
+    <!-- Contact 버튼 추가 -->
+    <div class="row justify-content-center mt-4">
+        <div class="col-6 text-center">
+            <button id="contact-button" class="btn btn-primary">Contact</button>
+        </div>
+    </div>
+
+    @livewireScripts
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0/js/bootstrap.bundle.min.js"></script>
+    <script>
+        document.getElementById('contact-button').addEventListener('click', function() {
+            window.location.href = '/contact'; // Contact 페이지로 이동
+        });
+    </script>
+
+    {{-- <div class="row justify-content-center">
+        <div class="col-6">
+            <h1>Contact</h1>
+        </div>
+    </div>
+
+    <div class="row justify-content-center">
+        <div class="col-6">
+            @livewire('LiveContact')
+        </div>
+    </div> --}}
+
 </x-www-layout>
