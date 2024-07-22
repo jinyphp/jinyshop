0d841ea285310dc8d8e4e1d9ab0f6bf60b7858ba
LTSGOD <lts0424@naver.com>
2024-07-22 22:56:13 +0900

    fix: .html" 제거

--- resources/www/shop-fashion-v2/_layouts/header.blade.php
+++ resources/www/shop-fashion-v2/_layouts/header.blade.php
@@ -496,7 +496,7 @@
                               <div class="position-relative d-flex flex-column justify-content-between h-100 z-2 pt-xl-2 ps-xl-2">
                                 <div class="h4 lh-base">Women's<br>Heels<br>Collection</div>
                                 <div>
-                                  <a class="btn btn-sm btn-dark stretched-link" href="shop-catalog-fashion.html" data-bs-theme="light">Shop now</a>
+                                  <a class="btn btn-sm btn-dark stretched-link" href="shop-catalog-fashion" data-bs-theme="light">Shop now</a>
                                 </div>
                               </div>
                               <img src="/assets/img/mega-menu/fashion/01.jpg" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover rtl-flip" alt="Image">
@@ -611,7 +611,7 @@
                               <div class="position-relative d-flex flex-column justify-content-between h-100 z-2 pt-xl-2 ps-xl-2">
                                 <div class="h4 lh-base">Men's<br>Sunglasses<br>Offers</div>
                                 <div>
-                                  <a class="btn btn-sm btn-dark stretched-link" href="shop-catalog-fashion.html" data-bs-theme="dark">Shop now</a>
+                                  <a class="btn btn-sm btn-dark stretched-link" href="shop-catalog-fashion" data-bs-theme="dark">Shop now</a>
                                 </div>
                               </div>
                               <img src="/assets/img/mega-menu/fashion/02.jpg" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover rtl-flip" alt="Image">
@@ -729,7 +729,7 @@
                               <div class="position-relative d-flex flex-column justify-content-between h-100 z-2 pt-xl-2 ps-xl-2">
                                 <div class="h4 lh-base">Kids'<br>Spring<br>Collection</div>
                                 <div>
-                                  <a class="btn btn-sm btn-dark stretched-link" href="shop-catalog-fashion.html" data-bs-theme="light">Shop now</a>
+                                  <a class="btn btn-sm btn-dark stretched-link" href="shop-catalog-fashion" data-bs-theme="light">Shop now</a>
                                 </div>
                               </div>
                               <img src="/assets/img/mega-menu/fashion/03.jpg" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover rtl-flip" alt="Image">
